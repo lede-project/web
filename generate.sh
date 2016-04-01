4f587e902b0a7b5967313ff6b9220246e4c45f0b
John Crispin <blogic@openwrt.org>
2016-04-01 14:13:55 +0200

    generate the docs
    Signed-off-by: John Crispin <blogic@openwrt.org>

--- generate.sh
+++ generate.sh
@@ -7,5 +7,6 @@
 
 D="$(pwd)"
 
-mkdir -p "$D/html"
+mkdir -p "$D/html/docs"
 find "$D/pages/" -type f | xargs -L1 a2x -f xhtml -r "$D/" -a "toc!" -a "numbered!" -D "$D/html/"
+find "$D/docs/" -type f | xargs -L1 a2x -f xhtml -r "$D/" -a "toc!" -a "numbered!" -D "$D/html/docs/"
